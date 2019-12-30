00c5310949982c35bccbc91ffc0cd53267553386
Oiar <643921472@qq.com>
2019-12-30 23:10:47 +0800

    kubernetes

--- http/main.go
+++ http/main.go
@@ -11,16 +11,16 @@ func myWeb(w http.ResponseWriter, r *http.Request) {
 		Value: "zhuzhu",
 	}
 	r.AddCookie(c)
-	fmt.Fprintln(w, r.UserAgent(), r.Cookies())
+	// fmt.Fprintln(w, r.UserAgent(), r.Cookies())
 	fmt.Fprintf(w, "这是一个开始")
 }
 
 func main() {
 	http.HandleFunc("/", myWeb) //注册路由函数
 
-	fmt.Println("服务器即将开启： http://127.0.0.1:8080")
+	fmt.Println("服务器即将开启： http://127.0.0.1:5001")
 
-	err := http.ListenAndServe(":8080", nil) //开始监听，处理请求，返回响应
+	err := http.ListenAndServe(":5001", nil) //开始监听，处理请求，返回响应
 	if err != nil {
 		fmt.Println("服务器开启错误: ", err)
 	}
